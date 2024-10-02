4465e1d6f7bd37845e650829403c5c543ec6e796
Javi Fdez <javifdez@eduroam031049.ugr.es>
2024-10-02 19:12:00 +0200

    Resolviendo problemas de incopatibilidad 2

--- game_stats.py
+++ game_stats.py
@@ -3,11 +3,10 @@ class GameStats:
 
     def __init__(self, ai_game):
         '''Inicializa las estadísticas'''
-
         self.settings = ai_game.settings
         self.game_active = True
         self.reset_stats()
 
     def reset_stats(self):
         '''Inicializa las estadísticas qe pueden cambiar durante el juego'''
-        self.ships_left = self.settings.ship_limit
+        self.ships_left = self.settings.ship_limit 
